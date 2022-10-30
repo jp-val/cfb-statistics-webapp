9ebd9041f61acfe5a3fb39cdca275560a78d76a6
jp val <valdespino.jp@knights.ucf.edu>
2022-10-29 23:23:43 -0400

    updated middleware authentication

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,51 +1,61 @@
-import SessionModal from '../models/session.js';
+import SessionModal from "../models/session.js";
+import { cleanupAddress } from "../other/ip.js";
+
+// 200 Ok
+// 400 Bad Request
+// 401 Unauthorized
+// 403 Forbidden
+// 404 Not Found
+// 500 Internal Error
 
 export const authenticate = async (req, res, next) => {
 
 	try {
 
-		const { ip, authToken } = req.body;
+		const { authToken } = req.cookies;
+		const ip = cleanupAddress(req.ip);
 
 		const session = await SessionModal.findOne({ authToken });
-		if (!session) return res.status(200).json({ message: 'Invalid authentication token.' });
+		if (!session) return res.status(401).json({ message: "Invalid authentication token." });
 
-		// if (ip != session.ip) {
-		// 	SessionModal.findByIdAndDelete(session._id);
-		// 	return res.status(200).json({ message: 'IP address does not match active session.' });
-		// }
+		if (ip != session.ip) {
+			await SessionModal.findByIdAndDelete(session._id);
+			return res.status(401).json({ message: "IP address does not match active session." });
+		}
 
 		var timeElapsed = Date.now() - session.loginTime;
 		var seconds = timeElapsed / 1000;
 		var minutes = seconds / 60;
 		var hours = minutes / 60;
 
-		console.log('hours passed:', hours);
+		console.log('ip:', ip, 'token:', session.authToken.substring(0, 10), 'hours passed:', hours);
 
 		if (hours > 6) {
-			SessionModal.findByIdAndDelete(session._id);
-			return res.status(200).json({ message: 'Authentication token expired.' });
+			res.clearCookie('authToken');
+			await SessionModal.findByIdAndDelete(session._id);
+			return res.status(401).json({ message: "Authentication token expired." });
 		}
 
-		req.body.id = session._id;
+		res.locals.id = session._id;
 
 		next();
 	
 	} catch (error) {
 
-		console.log('Something went wrong during authentication:', error.message);
-		return res.status(200).json({ message: error.message });
+		console.log("Internal server error while authenticating user:", error.message);
+		return res.status(500).json({ message: "Internal server error: " + error.message });
 	}
 };
 
-export const doNothing = async (req, res) => {
+export const finish = async (req, res) => {
 
 	try {
 
-		return res.status(200).json({ authenticated: true, message: 'Session authenticated.' });
+		return res.status(200).json({ authenticated: true });
 	
 	} catch (error) {
 
-		console.log('Something went wrong while doing nothing:', error.message);
-		return res.status(200).json({ message: error.message });
+		console.log("Internal server error while finishing:", error.message);
+		return res.status(500).json({ message: "Internal server error: " + error.message });
 	}
 };
