83942f9d8ae76695121bf84d461acac6a5f6d050
jp val <valdespino.jp@knights.ucf.edu>
2022-11-18 21:23:20 -0500

    updated navbar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -2,24 +2,12 @@ import Link from 'next/link';
 
 import styles from '../styles/Home.module.css';
 
-const pages = [ 
-	{ name: 'Home', link: '/' },
-  { name: 'Projects', link: '/projects' },
-  { name: 'Articles', link: '/articles' },
-  { name: 'About', link: '/about' },
-];
-
 const NavBar = () => {
 
 	return (
     <div>
       <div className={styles.logo}>
         <h1>JOSE PABLO</h1>
-      </div>
-      <div className={styles.navbar}>
-        {pages.map((page) => (
-          <Link key={page.name} href={page.link}><a>{page.name}</a></Link>
-        ))}
       </div>
     </div>
 	)
